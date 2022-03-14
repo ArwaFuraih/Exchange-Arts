f6dd77c32f3d6edf7f968892e532b7ba72370c0e
Arwa Alzahrani <89649823+Arwatech@users.noreply.github.com>
2022-03-14 09:39:39 +0300

    Add files via upload

---
+++ ArwaAlzahrani-Files/tagsCollectionView.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewCell.swift
+//  InspireMeProject
+//
+//  Created by Arwa Alzahrani on 05/08/1443 AH.
+//
+
+import UIKit
+
+class tagsCollectionView: UICollectionViewCell{
+    
+    @IBOutlet weak var CV: UIView!
+    @IBOutlet weak var BTNN: UIButton!
+  
+
+}
