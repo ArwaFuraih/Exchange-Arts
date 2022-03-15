f4017bbc4bbdd84a2b04f9a386eb553cad816a41
HananAlansari <97027503+HananAlansari@users.noreply.github.com>
2022-03-15 12:05:37 +0300

    Add files via upload

---
+++ HananAlansari-Files/Swift-Files/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Artist Profile3
+//
+//  Created by Hanan Alansari on 28/07/1443 AH.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photo: UIImageView!
+}
+
